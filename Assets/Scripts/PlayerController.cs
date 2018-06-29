b098264775507b803c5a34be5b218a491a230b38
Unknown <wndtanaka@gmail.com>
2018-06-29 21:00:03 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float speed = 5;
+    float inputH;
+    float inputV;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(inputH, 0, inputV) * speed * Time.deltaTime);
+    }
+}
