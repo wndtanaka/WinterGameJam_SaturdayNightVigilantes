cc66f9a0eed09c20a2a29aa0a3f976228470a50a
batmantafe <duane.reymond@gmail.com>
2018-06-30 10:04:22 +1000

    PlayerController script, both fists punching.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public PlayerType playerType;
 
     float speed = 5;
     float inputH;
@@ -15,7 +16,7 @@ public class PlayerController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        punchSpeed = 2f;
+        punchSpeed = 8f;
     }
 
     // Update is called once per frame
@@ -37,40 +38,43 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMovement()
     {
-        if (gameObject.name == "Player 1")
+
+        switch (playerType)
         {
-            //inputH = Input.GetAxis("Horizontal");
-            //inputV = Input.GetAxis("Vertical");
+            case PlayerType.PlayerOne:
+                //inputH = Input.GetAxis("Horizontal");
+                //inputV = Input.GetAxis("Vertical");
 
-            //transform.Translate(new Vector3(inputH, 0, inputV) * speed * Time.deltaTime);
+                //transform.Translate(new Vector3(inputH, 0, inputV) * speed * Time.deltaTime);
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
 
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Translate(-Vector3.forward * speed * Time.deltaTime);
-            }
-        }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+                }
+                break;
+            case PlayerType.PlayerTwo:
+                //inputH = Input.GetAxis("Horiz2");
+                //inputV = Input.GetAxis("Vert2");
 
-        if (gameObject.name == "Player 2")
-        {
-            //inputH = Input.GetAxis("Horiz2");
-            //inputV = Input.GetAxis("Vert2");
+                //transform.Translate(new Vector3(inputH, 0, inputV) * speed * Time.deltaTime);
 
-            //transform.Translate(new Vector3(inputH, 0, inputV) * speed * Time.deltaTime);
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
 
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.Translate(-Vector3.forward * speed * Time.deltaTime);
-            }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+                }
+                break;
+            default:
+                break;
         }
 
         if (otherPlayer != null)
@@ -81,30 +85,52 @@ public class PlayerController : MonoBehaviour
 
     void PlayerPunch()
     {
-        if (gameObject.name == "Player 1")
+        switch (playerType)
         {
-            if (Input.GetKey(KeyCode.G))
-            {
-                rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, punchTarget.transform.position, punchSpeed * Time.deltaTime);
-            }
+            case PlayerType.PlayerOne:
+                if (Input.GetKey(KeyCode.G))
+                {
+                    rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, punchTarget.transform.position, punchSpeed * Time.deltaTime);
+                }
 
-            if (!Input.GetKey(KeyCode.G))// && rightHand.transform.position != rHandOrigin.transform.position)
-            {
-                rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, rHandOrigin.transform.position, punchSpeed * Time.deltaTime);
-            }
-        }
+                if (!Input.GetKey(KeyCode.G))// && rightHand.transform.position != rHandOrigin.transform.position)
+                {
+                    rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, rHandOrigin.transform.position, punchSpeed / 2 * Time.deltaTime);
+                }
 
-        if (gameObject.name == "Player 2")
-        {
-            if (Input.GetKey(KeyCode.Keypad2))
-            {
-                rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, punchTarget.transform.position, punchSpeed * Time.deltaTime);
-            }
+                if (Input.GetKey(KeyCode.H))
+                {
+                    leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, punchTarget.transform.position, punchSpeed * Time.deltaTime);
+                }
 
-            if (!Input.GetKey(KeyCode.Keypad2))// && rightHand.transform.position != rHandOrigin.transform.position)
-            {
-                rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, rHandOrigin.transform.position, punchSpeed * Time.deltaTime);
-            }
+                if (!Input.GetKey(KeyCode.H))// && rightHand.transform.position != rHandOrigin.transform.position)
+                {
+                    leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, lHandOrigin.transform.position, punchSpeed / 2 * Time.deltaTime);
+                }
+                break;
+            case PlayerType.PlayerTwo:
+                if (Input.GetKey(KeyCode.Keypad2))
+                {
+                    rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, punchTarget.transform.position, punchSpeed * Time.deltaTime);
+                }
+
+                if (!Input.GetKey(KeyCode.Keypad2))// && rightHand.transform.position != rHandOrigin.transform.position)
+                {
+                    rightHand.transform.position = Vector3.MoveTowards(rightHand.transform.position, rHandOrigin.transform.position, punchSpeed / 2 * Time.deltaTime);
+                }
+
+                if (Input.GetKey(KeyCode.Keypad3))
+                {
+                    leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, punchTarget.transform.position, punchSpeed * Time.deltaTime);
+                }
+
+                if (!Input.GetKey(KeyCode.Keypad3))// && rightHand.transform.position != rHandOrigin.transform.position)
+                {
+                    leftHand.transform.position = Vector3.MoveTowards(leftHand.transform.position, lHandOrigin.transform.position, punchSpeed / 2 * Time.deltaTime);
+                }
+                break;
+            default:
+                break;
         }
     }
 }
